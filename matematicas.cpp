8e2255536896de29cf437b53e169fb59355a76dd
Nati393 <mvailatan@javerianacali.edu.co>
2025-08-26 03:54:40 -0500

    Update matematicas.cpp
    Implementar funciones

--- matematicas.cpp
+++ matematicas.cpp
@@ -13,4 +13,11 @@ int sumar(int a, int b) {
 int restar(int a, int b) {
     return a - b;
 }
+int multiplicar(int a, int b) {
+    return a * b;
+}
 
+int dividir(int a, int b) {
+    if (b == 0) return 0; // Evita división por cero
+    return a / b;
+}
