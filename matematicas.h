ba4db2d3da96858a016e0d68fd7064e1be20cee8
Nati393 <mvailatan@javerianacali.edu.co>
2025-08-26 03:53:39 -0500

    Update matematicas.h
    Agregar declaraciones de las nuevas funciones

--- matematicas.h
+++ matematicas.h
@@ -11,5 +11,7 @@
 // --- Declaraciones (El "que") --- (Aquí estamos haciendo definiciones)
 int sumar(int a, int b); // El ; es porque lo estamos llamando, la declaración
 int restar(int a, int b);
+int multiplicar(int a, int b);
+int dividir(int a, int b);
 
 #endif //CALCULADORA_MATEMATICAS_H
